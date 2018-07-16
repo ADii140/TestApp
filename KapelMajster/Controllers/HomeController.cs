931dd8f41aaaadb2414f3b6d71a79440fa09e137
Adrian <Dom>
2018-07-16 21:34:04 +0200

    initial commit

---
+++ KapelMajster/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KapelMajster.Models;
+
+namespace KapelMajster.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Tutaj możesz dodawać kategorie do bazy";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Kategorie()
+        { 
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Index(PrzenoszenieDanych dane)
+        {
+            string CategoryName = dane.CategoryName;
+            return View();
+        }
+    }
+}
---
+++ KapelMajster/Models/PrzenoszenieDanych.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KapelMajster.Models
+{
+    public class PrzenoszenieDanych
+    {
+        public string CategoryName { get; set; }
+
+    }
+}
